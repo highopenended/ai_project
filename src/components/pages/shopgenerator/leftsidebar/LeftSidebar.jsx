7f72b85d410f187c61beabfe614eed383fc59acf
Matt Glenn <highopenended@gmail.com>
2025-02-01 11:41:08 -0500

    WIP

--- src/components/pages/shopgenerator/leftsidebar/LeftSidebar.jsx
+++ src/components/pages/shopgenerator/leftsidebar/LeftSidebar.jsx
@@ -1,3 +1,20 @@
+/**
+ * LeftSidebar Component
+ * 
+ * STYLING RULES:
+ * 1. This component's CSS should ONLY contain:
+ *    - Main sidebar container styling (.shop-generator-sidebar)
+ *    - Single parameter section wrapper (.parameter-section)
+ *    - Parameter sections container (.parameter-sections)
+ * 
+ * 2. Each child component MUST:
+ *    - Have its own CSS file
+ *    - Be completely self-contained in styling
+ *    - Not rely on parent styles beyond the wrapper
+ * 
+ * 3. Child components should be easily rearrangeable
+ *    within the parameter-sections container
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 import './LeftSidebar.css';
@@ -14,7 +31,9 @@ function LeftSidebar({ children, onGenerate }) {
         <div className="shop-generator-sidebar">
             <h2>Shop Generator</h2>
             <GenerateButton onClick={handleGenerateClick} />
+            {/* This container should only provide basic layout structure */}
             <div className="parameter-sections">
+                {/* Each child gets wrapped in a standard section container */}
                 {React.Children.map(children, (child, index) => (
                     <div className="parameter-section" key={index}>
                         {child}
