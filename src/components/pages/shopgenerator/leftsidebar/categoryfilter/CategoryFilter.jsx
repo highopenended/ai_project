7352bdfd95aec9bc425836642d009d7e135275b8
Matt Glenn <highopenended@gmail.com>
2025-02-04 18:50:22 -0500

    WIP

--- src/components/pages/shopgenerator/leftsidebar/categoryfilter/CategoryFilter.jsx
+++ src/components/pages/shopgenerator/leftsidebar/categoryfilter/CategoryFilter.jsx
@@ -2,22 +2,18 @@ import { useState } from 'react';
 import { useCategoryContext, SELECTION_STATES } from '../../context/CategoryContext';
 import './CategoryFilter.css';
 import DropdownArrow from '../../components/DropdownArrow';
+import SubcategoryFilter from '../subcategoryfilter/SubcategoryFilter';
 
 function CategoryFilter() {
     const {
         categoryData,
         getCategoryState,
-        getSubcategoryState,
         toggleCategory,
-        toggleSubcategory,
-        clearCategorySelections,
-        clearSubcategorySelections
+        clearCategorySelections
     } = useCategoryContext();
 
     const [categoryFilter, setCategoryFilter] = useState('');
-    const [subcategoryFilter, setSubcategoryFilter] = useState('');
     const [isCategoryCollapsed, setIsCategoryCollapsed] = useState(false);
-    const [isSubcategoryCollapsed, setIsSubcategoryCollapsed] = useState(false);
 
     // Filter categories based on search
     const filteredCategories = Object.keys(categoryData)
@@ -25,30 +21,6 @@ function CategoryFilter() {
             category.toLowerCase().includes(categoryFilter.toLowerCase())
         )
         .sort();
-
-    // Get all unique subcategories from selected categories
-    const getRelevantSubcategories = () => {
-        const includedCategories = Object.keys(categoryData).filter(
-            cat => getCategoryState(cat) === SELECTION_STATES.INCLUDE
-        );
-
-        if (includedCategories.length === 0) {
-            return Object.values(categoryData)
-                .flatMap(cat => cat.subcategories)
-                .filter((value, index, self) => self.indexOf(value) === index)
-                .sort();
-        }
-        return includedCategories
-            .flatMap(category => categoryData[category]?.subcategories || [])
-            .filter((value, index, self) => self.indexOf(value) === index)
-            .sort();
-    };
-
-    // Filter subcategories based on search
-    const filteredSubcategories = getRelevantSubcategories()
-        .filter(subcategory => 
-            subcategory.toLowerCase().includes(subcategoryFilter.toLowerCase())
-        );
 
     const getTagClassName = (state) => {
         const baseClass = 'tag';
@@ -112,57 +84,7 @@ function CategoryFilter() {
                 )}
             </div>
 
-            <div className="subcategory-section">
-                <div className="section-header">
-                    <h3>Subcategories</h3>
-                    <div className="buttons">
-                        <button 
-                            className="reset-button" 
-                            onClick={clearSubcategorySelections}
-                            title="Reset subcategory selections"
-                        >
-                            <svg 
-                                width="16" 
-                                height="16" 
-                                viewBox="0 0 24 24" 
-                                fill="none" 
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path 
-                                    d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
-                                    fill="currentColor"
-                                />
-                            </svg>
-                        </button>
-                        <DropdownArrow isCollapsed={isSubcategoryCollapsed} toggleCollapse={() => setIsSubcategoryCollapsed(!isSubcategoryCollapsed)} />
-                    </div>
-                </div>
-                {!isSubcategoryCollapsed && (
-                    <>
-                        <input
-                            type="text"
-                            placeholder="Search subcategories..."
-                            value={subcategoryFilter}
-                            onChange={(e) => setSubcategoryFilter(e.target.value)}
-                            className="search-input"
-                        />
-                        <div className="tag-container">
-                            {filteredSubcategories.map(subcategory => (
-                                <button
-                                    key={subcategory}
-                                    className={getTagClassName(getSubcategoryState(subcategory))}
-                                    onClick={() => toggleSubcategory(subcategory)}
-                                >
-                                    {subcategory}
-                                    {getSubcategoryState(subcategory) === SELECTION_STATES.EXCLUDE && (
-                                        <span className="exclude-indicator">✕</span>
-                                    )}
-                                </button>
-                            ))}
-                        </div>
-                    </>
-                )}
-            </div>
+            <SubcategoryFilter />
         </div>
     );
 }
