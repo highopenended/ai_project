2ea495a55722b64eeaf180d7612ac5952510a19e
Matt Glenn <highopenended@gmail.com>
2025-02-04 19:25:59 -0500

    WIP

--- src/components/pages/shopgenerator/leftsidebar/subcategoryfilter/SubcategoryFilter.jsx
+++ src/components/pages/shopgenerator/leftsidebar/subcategoryfilter/SubcategoryFilter.jsx
@@ -3,6 +3,7 @@ import { useCategoryContext, SELECTION_STATES } from '../../context/CategoryCont
 import './SubcategoryFilter.css';
 import DropdownArrow from '../../components/DropdownArrow';
 import TagContainer from '../../components/TagContainer';
+import ResetButton from '../../components/ResetButton';
 
 function SubcategoryFilter() {
     const {
@@ -53,24 +54,10 @@ function SubcategoryFilter() {
             <div className="section-header">
                 <h3>Subcategories</h3>
                 <div className="buttons">
-                    <button 
-                        className="reset-button" 
+                    <ResetButton 
                         onClick={clearSubcategorySelections}
                         title="Reset subcategory selections"
-                    >
-                        <svg 
-                            width="16" 
-                            height="16" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path 
-                                d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
-                                fill="currentColor"
-                            />
-                        </svg>
-                    </button>
+                    />
                     <DropdownArrow isCollapsed={isSubcategoryCollapsed} toggleCollapse={() => setIsSubcategoryCollapsed(!isSubcategoryCollapsed)} />
                 </div>
             </div>
--- src/components/pages/shopgenerator/leftsidebar/traitfilter/TraitFilter.jsx
+++ src/components/pages/shopgenerator/leftsidebar/traitfilter/TraitFilter.jsx
@@ -4,6 +4,7 @@ import traitList from '../../../../../../public/trait-list.json';
 import './TraitFilter.css';
 import DropdownArrow from '../../components/DropdownArrow';
 import TagContainer from '../../components/TagContainer';
+import ResetButton from '../../components/ResetButton';
 
 function TraitFilter() {
     const {
@@ -38,24 +39,10 @@ function TraitFilter() {
                 <div className="section-header">
                     <h3>Traits</h3>
                     <div className="buttons">
-                        <button 
-                            className="reset-button" 
+                        <ResetButton 
                             onClick={clearTraitSelections}
                             title="Reset trait selections"
-                        >
-                            <svg 
-                                width="16" 
-                                height="16" 
-                                viewBox="0 0 24 24" 
-                                fill="none" 
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path 
-                                    d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
-                                    fill="currentColor"
-                                />
-                            </svg>
-                        </button>
+                        />
                         <button
                             className={`collapse-button ${isCollapsed ? 'collapsed' : ''}`}
                             onClick={() => setIsCollapsed(!isCollapsed)}
