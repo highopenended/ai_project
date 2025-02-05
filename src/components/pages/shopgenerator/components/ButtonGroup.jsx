c2e2b4166290086033dd9692f49ce99535a780ed
Matt Glenn <highopenended@gmail.com>
2025-02-05 15:17:47 -0500

    WIP

--- src/components/pages/shopgenerator/components/ButtonGroup.jsx
+++ src/components/pages/shopgenerator/components/ButtonGroup.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 export default function ButtonGroup({ handleReset, isCollapsed, setIsCollapsed }) {
     return (
         <div className="buttons">
-            <ResetButton onClick={handleReset} title="Reset Values" />
+            {handleReset && <ResetButton onClick={handleReset} title="Reset Values" />}
             <CollapseExpandButton isCollapsed={isCollapsed} toggleCollapse={() => setIsCollapsed(!isCollapsed)} />
         </div>
     );
--- src/components/pages/shopgenerator/rightsidebar/ActionButtonsSection.jsx
+++ src/components/pages/shopgenerator/rightsidebar/ActionButtonsSection.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 import Section from '../components/Section';
-
+import ButtonGroup from '../components/ButtonGroup';
+import { useState } from 'react';
 const ActionButtonsSection = ({ onGenerate, onSave, areAllDetailsFilled }) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
     return (
-        <Section title="Actions">
-            <button 
+        <Section title="Actions" 
+        buttonGroup={<ButtonGroup isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} /> }
+        >
+            {/* <button 
                 className="action-button"
                 onClick={onGenerate}
                 aria-label="Generate Shop Details"
             >
                 Generate Shop Details
-            </button>
+            </button> */}
             <button 
                 className="action-button" 
                 onClick={onSave} 
