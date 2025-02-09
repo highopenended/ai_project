a971ccee9e91fd53439222edbe202fb01d6e12fb
Matt Glenn <highopenended@gmail.com>
2025-02-09 14:43:13 -0500

    WIP

--- src/components/pages/newtest/NewTest.jsx
+++ src/components/pages/newtest/NewTest.jsx
@@ -4,10 +4,14 @@ import TabContainer from './components/TabContainer';
 import Tab1 from './tabs/Tab1';
 import Tab2 from './tabs/Tab2';
 import Tab3 from './tabs/tab3';
+import Tab4 from './tabs/Tab4';
+
+
+
 
 function NewTest() {
     const [tabGroups, setTabGroups] = useState([
-        [<Tab1 key="Tab1-0" />, <Tab2 key="Tab2-0" />, <Tab3 key="Tab3-0" />]
+        [<Tab1 key="Tab1-0" />, <Tab2 key="Tab2-0" />, <Tab3 key="Tab3-0" />, <Tab4 key="Tab4-0" />]
     ]);
 
     const handleTabMove = (newTabs) => {
---
+++ src/components/pages/newtest/tabs/Tab4.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+function Tab4() {
+    return <div>Tab 4 Content</div>;
+}
+
+export default Tab4;
