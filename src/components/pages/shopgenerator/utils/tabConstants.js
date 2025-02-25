b2fdfac81065aa085636305766cb856cab15cdbe
Matt Glenn <highopenended@gmail.com>
2025-02-25 18:30:44 -0500

    Set new default shop generator tab layout

--- src/components/pages/shopgenerator/utils/tabConstants.js
+++ src/components/pages/shopgenerator/utils/tabConstants.js
@@ -7,12 +7,10 @@ export const TAB_TYPE_IDENTIFIERS = {
 
 export const DEFAULT_TAB_STATE = {
     groups: [
-        [
-            { type: TAB_TYPE_IDENTIFIERS.PARAMETERS, key: "Tab_Parameters-0" },
-            { type: TAB_TYPE_IDENTIFIERS.INVENTORY, key: "Tab_InventoryTable-0" },
-            { type: TAB_TYPE_IDENTIFIERS.CHOOSE_SHOP, key: "Tab_ChooseShop-0" },
-            { type: TAB_TYPE_IDENTIFIERS.SHOP_DETAILS, key: "Tab_ShopDetails-0" },
-        ]
+        [{ type: TAB_TYPE_IDENTIFIERS.PARAMETERS, key: "Tab_Parameters-0" }],
+        [{ type: TAB_TYPE_IDENTIFIERS.INVENTORY, key: "Tab_InventoryTable-0" }],
+        [{ type: TAB_TYPE_IDENTIFIERS.CHOOSE_SHOP, key: "Tab_ChooseShop-0" }],
+        [{ type: TAB_TYPE_IDENTIFIERS.SHOP_DETAILS, key: "Tab_ShopDetails-0" }]
     ],
-    widths: ["100%"]
+    widths: ["10%", "55%", "10%", "20%"]
 }; 
